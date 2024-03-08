00c821282c5bdc631792c0c0ae9ce683aeed8b6f
Gennadii <Gennadii_s@inbox.ru>
2024-03-09 00:27:17 +1000

    build

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
-import App from "./componets/App.jsx";
-
-const rootElement = document.getElementById('root');
-
-ReactDOM.createRoot(rootElement).render(<App />);
